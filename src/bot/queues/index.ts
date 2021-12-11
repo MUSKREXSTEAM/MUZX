8c5f2f2a75b3194502c916875c90a9b024773290
MUSKREXSTEAM <MUSKREXSTEAM@users.noreply.github.com>
2021-12-11 15:20:01 +0000

    Format files

--- src/bot/queues/index.ts
+++ src/bot/queues/index.ts
@@ -1,114 +1,114 @@
-import { Readable } from 'stream'
-import { User } from '@grammyjs/types'
+import { Readable } from "stream";
+import { User } from "@grammyjs/types";
 
 export interface Item {
-  getReadable: () => Promise<Readable> | Readable
-  url: string
-  title: string
-  requester: User
+  getReadable: () => Promise<Readable> | Readable;
+  url: string;
+  title: string;
+  requester: User;
 }
 
-export type NowHandler = (chatId: number, now: Item) => Promise<void>
+export type NowHandler = (chatId: number, now: Item) => Promise<void>;
 
 export default new (class Queues {
-  queues: Map<number, Item[]> = new Map()
-  now: Map<number, Item> = new Map()
-  nowHandlers: NowHandler[] = []
+  queues: Map<number, Item[]> = new Map();
+  now: Map<number, Item> = new Map();
+  nowHandlers: NowHandler[] = [];
 
   addNowHandler(handler: NowHandler) {
-    this.nowHandlers.push(handler)
+    this.nowHandlers.push(handler);
   }
 
   setNow(chatId: number, item: Item) {
-    this.now.set(chatId, item)
-    this.nowHandlers.forEach(handler => handler(chatId, item))
+    this.now.set(chatId, item);
+    this.nowHandlers.forEach((handler) => handler(chatId, item));
   }
 
   rmNow(chatId: number) {
-    return this.now.delete(chatId)
+    return this.now.delete(chatId);
   }
 
   getNow(chatId: number) {
-    return this.now.get(chatId)
+    return this.now.get(chatId);
   }
 
   getNext(chatId: number) {
-    return this.queues.get(chatId)?.[0]
+    return this.queues.get(chatId)?.[0];
   }
 
   push(chatId: number, item: Item) {
-    const queue = this.queues.get(chatId)
+    const queue = this.queues.get(chatId);
 
     if (queue) {
-      queue.push(item)
-      return queue.length
+      queue.push(item);
+      return queue.length;
     }
 
-    this.queues.set(chatId, [item])
-    return 1
+    this.queues.set(chatId, [item]);
+    return 1;
   }
 
   get(chatId: number) {
-    const queue = this.queues.get(chatId)
+    const queue = this.queues.get(chatId);
 
-    this.rmNow(chatId)
+    this.rmNow(chatId);
 
     if (queue) {
-      const item = queue.shift()
+      const item = queue.shift();
 
       if (item) {
-        this.setNow(chatId, item)
+        this.setNow(chatId, item);
       }
 
-      return item
+      return item;
     }
   }
 
   getAll(chatId: number) {
-    const queue = this.queues.get(chatId)
+    const queue = this.queues.get(chatId);
 
     if (queue) {
-      return queue
+      return queue;
     }
 
-    return []
+    return [];
   }
 
   clear(chatId: number) {
-    this.rmNow(chatId)
+    this.rmNow(chatId);
 
     if (this.queues.has(chatId)) {
-      this.queues.set(chatId, [])
-      return true
+      this.queues.set(chatId, []);
+      return true;
     }
 
-    return false
+    return false;
   }
 
   suffle(chatId: number) {
-    const now = this.getNow(chatId)
+    const now = this.getNow(chatId);
 
     if (!now || this.getAll(chatId).length == 0) {
-      return false
+      return false;
     }
 
-    this.push(chatId, now)
+    this.push(chatId, now);
 
-    const items = this.getAll(chatId)
+    const items = this.getAll(chatId);
 
     let currentIndex = items.length,
-      randomIndex
+      randomIndex;
 
     while (currentIndex != 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex)
-      currentIndex--
-      ;[items[currentIndex], items[randomIndex]] = [
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      [items[currentIndex], items[randomIndex]] = [
         items[randomIndex],
-        items[currentIndex]
-      ]
+        items[currentIndex],
+      ];
     }
 
-    this.queues.set(chatId, items)
-    return this.get(chatId)!
+    this.queues.set(chatId, items);
+    return this.get(chatId)!;
   }
-})()
+})();
