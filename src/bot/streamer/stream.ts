8718c2c5c6aa1bc2da697ac88a7a1e76a73381b2
MUSKREXSTEAM <MUSKREXSTEAM@users.noreply.github.com>
2021-12-11 15:20:34 +0000

    Format files

--- src/bot/streamer/stream.ts
+++ src/bot/streamer/stream.ts
@@ -1,61 +1,61 @@
-import gramtgcalls from '../../userbot/gramtgcalls'
-import queues from '../queues'
-import { Item } from '../queues'
-import { loop } from '../cache'
+import gramtgcalls from "../../userbot/gramtgcalls";
+import queues from "../queues";
+import { Item } from "../queues";
+import { loop } from "../cache";
 
-const stopped = new Map<number, boolean>()
+const stopped = new Map<number, boolean>();
 
 export const next = (chatId: number, force?: boolean) => async () => {
   if (stopped.get(chatId)) {
-    return
+    return;
   }
 
   if (loop.get(chatId) && !force) {
-    const now = queues.getNow(chatId)
+    const now = queues.getNow(chatId);
 
     if (now) {
-      await stream(chatId, now)
-      return true
+      await stream(chatId, now);
+      return true;
     }
   }
 
-  const item = queues.get(chatId)
+  const item = queues.get(chatId);
 
   if (item) {
-    await stream(chatId, item, true)
-    return true
+    await stream(chatId, item, true);
+    return true;
   }
 
-  stopped.set(chatId, true)
-  return await gramtgcalls(chatId).stop()
-}
+  stopped.set(chatId, true);
+  return await gramtgcalls(chatId).stop();
+};
 
 export async function stream(chatId: number, item: Item, force?: boolean) {
-  const finished = gramtgcalls(chatId).audioFinished != false
+  const finished = gramtgcalls(chatId).audioFinished != false;
 
   if (finished || force) {
-    const getReadableResult = item.getReadable()
+    const getReadableResult = item.getReadable();
 
     const readable =
       getReadableResult instanceof Promise
         ? await getReadableResult
-        : getReadableResult
+        : getReadableResult;
 
     await gramtgcalls(chatId).stream(
       {
         readable,
-        listeners: { onFinish: next(chatId) }
+        listeners: { onFinish: next(chatId) },
       },
       undefined,
       { join: { videoStopped: true } }
-    )
-    stopped.set(chatId, false)
+    );
+    stopped.set(chatId, false);
 
-    queues.setNow(chatId, item)
+    queues.setNow(chatId, item);
 
-    return null
+    return null;
   } else {
-    const position = queues.push(chatId, item)
-    return position
+    const position = queues.push(chatId, item);
+    return position;
   }
 }
