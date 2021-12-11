faf81153539efaad7293518f1445365706a2a8d2
MUSKREXSTEAM <89529009+MUSKREXSTEAM@users.noreply.github.com>
2021-12-11 18:15:57 +0300

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,4 @@
-import { config } from "dotenv";
-import { cleanEnv, num, str } from "envalid";
+import userbot from './userbot'
+import bot from './bot'
 
-config();
-
-export default cleanEnv(process.env, {
-  BOT_TOKEN: str(),
-  STRING_SESSION: str(),
-  API_ID: num(),
-  API_HASH: str(),
-  LOCALE: str({ default: "en" }),
-  MAX_PLAYLIST_SIZE: num({ default: 10 }),
-  COOKIES: str({ default: "" }),
-});
+Promise.all([userbot(), bot()])
